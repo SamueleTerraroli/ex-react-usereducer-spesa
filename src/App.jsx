2e2b06a97bd09a54bdfe03dd02c8b3b6cbb04e49
SamueleTerraroli <terraroli99@gmail.com>
2025-04-10 16:18:50 +0200

    add: milestone 1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+function App() {
+
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+  return (
+    <>
+      <h1>Lista prodotti</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price.toFixed(2)}€
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
