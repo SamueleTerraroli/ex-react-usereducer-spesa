9c66fdf7fdae52ddf882bdc05b9987cd13759c53
SamueleTerraroli <terraroli99@gmail.com>
2025-04-10 16:34:59 +0200

    add: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,21 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts);
+
+  const addToCart = (product) => {
+    const isProductInCart = addedProducts.some(item => item.name === product.name);
+    if (isProductInCart) {
+      return;
+    }
+    const productToAdd = {
+      ...product,
+      quantity: 1
+    }
+    setAddedProducts(prevProducts => [...prevProducts, productToAdd]);
+  }
+
   return (
     <>
       <h1>Lista prodotti</h1>
@@ -16,9 +31,20 @@ function App() {
         {products.map((product, index) => (
           <li key={index}>
             {product.name} - {product.price.toFixed(2)}€
+            <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
+      {addedProducts.length > 0 && (<>
+        <h1>Carrello</h1>
+        <ul>
+          {addedProducts.map((product, index) => (
+            <li key={index}>
+              {product.name} - {product.price.toFixed(2)}€ x {product.quantity}
+            </li>
+          ))}
+        </ul>
+      </>)}
     </>
   )
 }
